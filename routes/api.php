3e1bc909a8cf9ff34635dda92209d1024c3856b1
daniel <singiandaniel@gmail.com>
2024-10-21 21:05:11 +0800

    auth

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Define the route for fetching posts
+Route::middleware('auth:sanctum')->get('/posts', [PostsController::class, 'browse']);
